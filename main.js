3c3374e4291dd4939ff5b662ff96284aa3c1206b
Lane Garner <b.lane.garner@gmail.com>
2021-01-12 11:44:14 -0600

    added resize event listeners for mobile menu

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const veggieBurger = document.getElementById("veggie-burger");
 const veggieBurgerExpanded = document.getElementById("veggie-burger-expanded");
 const mobileNav = document.getElementById("mobile-nav");
+const x = window.matchMedia("(max-width: 800px)")
 
 
 mobileNav.style.display = "none";
@@ -19,10 +20,46 @@ veggieBurgerExpanded.addEventListener("click", () => {
 })
 
 document.addEventListener("scroll", () => {
+    if (x.matches) {
     mobileNav.style.display = "none";
     veggieBurgerExpanded.style.display = "none";
     veggieBurger.style.display = "inline";
+    }
 })
+
+window.addEventListener("resize", () => {
+    if (!x.matches) {
+        mobileNav.style.display = "none";
+        veggieBurgerExpanded.style.display = "none";
+        veggieBurger.style.display = "none";
+    }
+})
+
+window.addEventListener("resize", () => {
+    if (x.matches) {
+        mobileNav.style.display = "none";
+        veggieBurgerExpanded.style.display = "none";
+        veggieBurger.style.display = "inline";
+    }
+})
+
+
+
+
+// function myFunction(x) {
+//     if (x.matches) { // If media query matches
+//       document.body.style.backgroundColor = "yellow";
+//     } else {
+//      document.body.style.backgroundColor = "pink";
+//     }
+//   }
+  
+//   const x = window.matchMedia("(max-width: 800px)")
+//   myFunction(x) // Call listener function at run time
+//   x.addListener(myFunction) // Attach listener function on state changes
+
+
+
 
 
 // }
